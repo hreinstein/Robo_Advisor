b964ce785e330b9e94696f9f7c3c95fee78238a7
hreinstein <59658346+hreinstein@users.noreply.github.com>
2020-02-18 18:07:15 -0500

    imported packages, created .gitignore, created API Key variables

--- app/robo.py
+++ app/robo.py
@@ -1,5 +1,34 @@
 # app/robo.py
 
+import requests
+import json
+import os 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+print("REQUESTING SOME DATA FROM THE INTERNET...")
+
+API_KEY = os.getenv("ALPHAVANTAGE_API_KEY", default = "OOPS") 
+
+symbol = "TSLA" # todo: ask for user input 
+
+request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={API_KEY}"
+print("URL:", request_url)
+
+response = requests.get(request_url)
+print(type(response))
+print(response.status_code)
+print(type(response.text)) #> str 
+parsed_response = json.loads(response.text)
+print(type(parsed_response)) #> dict 
+print(parsed_response)
+
+#handle response errors: 
+if "Error Messge" in response.text:
+    print("Sorry, could not find that symbol, please try again")
+    exit()
+
 print("-------------------------")
 print("SELECTED SYMBOL: XYZ")
 print("-------------------------")
@@ -16,3 +45,4 @@ print("RECOMMENDATION REASON: TODO")
 print("-------------------------")
 print("HAPPY INVESTING!")
 print("-------------------------")
+
