ad68df1991dfebddec83ec7a0e30302612f73a00
hreinstein <59658346+hreinstein@users.noreply.github.com>
2020-02-19 16:51:20 -0500

    get low prices

--- app/robo.py
+++ app/robo.py
@@ -46,12 +46,17 @@ latest_close = tsd[latest_day]["4. close"] #> 1,000.00
 
 # maximum of all high prices
 high_prices = []
+low_prices = []
+
 
 for date in dates: 
     high_price = tsd[date]["2. high"]
+    low_price = tsd[date]["3. low"]
     high_prices.append(float(high_price))
+    low_prices.append(float(low_price))
 
 recent_high = max(high_prices)
+recent_low = min(low_prices)
 
 #handle response errors: 
 if "Error Messge" in response.text:
@@ -67,7 +72,7 @@ print("-------------------------")
 print(f"LATEST DAY: {last_refreshed}")
 print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
 print(f"RECENT HIGH: {to_usd(float(recent_high))}") # maximum of all daily high prices
-print("RECENT LOW: $99,000.00")
+print(f"RECENT LOW: {to_usd(float(recent_low))}")
 print("-------------------------")
 print("RECOMMENDATION: BUY!")
 print("RECOMMENDATION REASON: TODO")
