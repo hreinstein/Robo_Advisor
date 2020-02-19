6bbef66df69d639bfa8df974904539f6df71b949
hreinstein <59658346+hreinstein@users.noreply.github.com>
2020-02-19 17:39:21 -0500

    writing some example data to csv

--- app/robo.py
+++ app/robo.py
@@ -2,17 +2,18 @@
 
 import requests
 import json
+import csv
 import os 
 from dotenv import load_dotenv
 load_dotenv()
 import datetime
 
 
-#format to usd
+# format to usd
 def to_usd(my_price):
     return "${0:.2f}".format(my_price) #> $10,000.00
 
-# Ino inputs
+# Info inputs
 
 print("REQUESTING SOME DATA FROM THE INTERNET...")
 
@@ -26,16 +27,13 @@ print("URL:", request_url)
 response = requests.get(request_url)
 print(type(response)) #> <class 'requests.models.Response'>
 print(response.status_code) #> 200
-#print(type(response.text)) #> str, need to parse this to make into a dictionary below 
 
 parsed_response = json.loads(response.text)
 print(type(parsed_response)) #> dict 
 print(parsed_response)
 
-
 # compute lastest day 
 last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
-
 
 # compute latest close
 tsd = parsed_response["Time Series (Daily)"]
@@ -43,8 +41,7 @@ dates = list(tsd.keys()) # TODO: assumes the first day in on top, sort to ensure
 latest_day = dates[0] #"2020-02-19"
 latest_close = tsd[latest_day]["4. close"] #> 1,000.00
 
-
-# maximum of all high prices
+# compute high and low prices 
 high_prices = []
 low_prices = []
 
@@ -58,10 +55,23 @@ for date in dates:
 recent_high = max(high_prices)
 recent_low = min(low_prices)
 
-#handle response errors: 
+
+# handle response errors: 
 if "Error Messge" in response.text:
     print("Sorry, could not find that symbol, please try again")
     exit() 
+
+# csv 
+#csv_file_path = "data/prices.csv" # a relative filepath
+csv_file_path = os.path.join(os.path.dirname(__file__), "..", "data", "prices.csv")
+with open(csv_file_path, "w") as csv_file: # "w" means "open the file for writing"
+    writer = csv.DictWriter(csv_file, fieldnames=["city", "name"])
+    writer.writeheader() # uses fieldnames set above
+    writer.writerow({"city": "New York", "name": "Yankees"})
+    writer.writerow({"city": "New York", "name": "Mets"})
+    writer.writerow({"city": "Boston", "name": "Red Sox"})
+    writer.writerow({"city": "New Haven", "name": "Ravens"})
+
 
 print("-------------------------")
 print("SELECTED SYMBOL: XYZ")
@@ -77,6 +87,9 @@ print("-------------------------")
 print("RECOMMENDATION: BUY!")
 print("RECOMMENDATION REASON: TODO")
 print("-------------------------")
+print(f"WRITING DATA TO CSV: {csv_file_path} ...")
+print("-------------------------")
 print("HAPPY INVESTING!")
 print("-------------------------")
+
 
