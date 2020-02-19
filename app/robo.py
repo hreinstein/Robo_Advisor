9778685f818c900192d399ccf48579e431e2f73e
hreinstein <59658346+hreinstein@users.noreply.github.com>
2020-02-19 16:47:06 -0500

    get latest high price

--- app/robo.py
+++ app/robo.py
@@ -4,19 +4,22 @@ import requests
 import json
 import os 
 from dotenv import load_dotenv
+load_dotenv()
 import datetime
 
-load_dotenv()
 
+#format to usd
 def to_usd(my_price):
     return "${0:.2f}".format(my_price) #> $10,000.00
+
+# Ino inputs
 
 print("REQUESTING SOME DATA FROM THE INTERNET...")
 
 API_KEY = os.getenv("ALPHAVANTAGE_API_KEY", default = "OOPS") 
-
 symbol = "TSLA" # todo: ask for user input 
 
+# request API url 
 request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={API_KEY}"
 print("URL:", request_url)
 
@@ -29,12 +32,26 @@ parsed_response = json.loads(response.text)
 print(type(parsed_response)) #> dict 
 print(parsed_response)
 
+
+# compute lastest day 
 last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
 
+
+# compute latest close
 tsd = parsed_response["Time Series (Daily)"]
 dates = list(tsd.keys()) # TODO: assumes the first day in on top, sort to ensure that the latest day is first 
-latest_day = dates[0] #"2020-02-20"
+latest_day = dates[0] #"2020-02-19"
 latest_close = tsd[latest_day]["4. close"] #> 1,000.00
+
+
+# maximum of all high prices
+high_prices = []
+
+for date in dates: 
+    high_price = tsd[date]["2. high"]
+    high_prices.append(float(high_price))
+
+recent_high = max(high_prices)
 
 #handle response errors: 
 if "Error Messge" in response.text:
@@ -49,7 +66,7 @@ print("REQUEST AT: 2018-02-20 02:00pm") #TODO: use date time module
 print("-------------------------")
 print(f"LATEST DAY: {last_refreshed}")
 print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
-print("RECENT HIGH: $101,000.00")
+print(f"RECENT HIGH: {to_usd(float(recent_high))}") # maximum of all daily high prices
 print("RECENT LOW: $99,000.00")
 print("-------------------------")
 print("RECOMMENDATION: BUY!")
